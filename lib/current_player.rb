12b063b72044ef40015d674c664ccd0fbc9b785c
Amedeo d'Amore <amedeodamore@gmail.com>
2021-01-17 23:42:22 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,9 +2,9 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def turn_count(board)
 counter = 0
-board.each_char { |position| if position == "X" || position == "O" turns += 1}
+board.each_char { |position| if position == "X" || position == "O" counter += 1}
 end
-turns
+counter
 
 def current_player
 
